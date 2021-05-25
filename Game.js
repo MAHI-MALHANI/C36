ae7c01839054dd65269a5d24f6cb21749a9662a3
MAHI-MALHANI <76902982+MAHI-MALHANI@users.noreply.github.com>
2021-05-25 22:41:49 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,26 +1,65 @@
-class Game {
-  constructor(){}
-  
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
-   
-  }
+class Game{
 
-  update(state){
-    database.ref('/').update({
-      gameState: state
-    });
-  }
+    constructor(){}
 
-  start(){
-    if(gameState === 0){
-      player = new Player();
-      player.getCount();
-      form = new Form()
-      form.display();
-    }
+    getState(){
+      var gameStateRef=database.ref('gameState');
+      gameStateRef.on('value', function (data){
+          gameState=data.val();
+
+      })
+    
+}
+
+update(state){
+database.ref('/').update({
+    gameState: state
+    
+});
+}
+
+async start(){
+   if(gameState===0){
+       player=new Player();
+
+       var playerCountRef=await database.ref('playerCount').once("value");
+      //Listens for exactly one event of the specified event type, and then stops listening.
+      //This is equivalent to calling on(), and then calling off() inside the callback function. S
+       if(playerCountRef.exists()){
+         playerCount=playerCountRef.val();
+
+       player.getCount();
+       }
+
+       form=new Form();
+       form.display();
+   }
+}
+
+play(){
+  form.hide();
+  textSize(30);
+  text("Game Start", 120, 100);
+  Player.getPlayerInfo();
+  if(allPlayers !== undefined){
+      var display_position=130;
+
+      for (var plr in allPlayers){
+
+        if(plr==="player" +player.index)
+            fill("red");
+            else
+                fill("black");
+
+                display_position+=20;
+                textSize(15);
+                text(allPlayers[plr]. name+ ": " + allPlayers[plr].distane, 120, display_position);  
+        }
+      }
+      if(keyIsDown(UP_ARROW) && player.index !== null){
+          player.distance+=50;
+          player.update();
   }
+}
+
 }
--- Player.js
+++ Player.js
@@ -1,23 +1,39 @@
-class Player {
-  constructor(){}
+class Player{
+    constructor(){
+        this.index= null;
+        this.distance=0;
+        this.name=null;
+    }
 
-  getCount(){
-    var playerCountRef = database.ref('playerCount');
-    playerCountRef.on("value",function(data){
-      playerCount = data.val();
-    })
-  }
+    getCount(){
+        var playerCountRef=database.ref('playerCount');
+        playerCountRef.on("value", function(data){
 
-  updateCount(count){
-    database.ref('/').update({
-      playerCount: count
+            playerCount=data.val();
+
+        });
+
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+           playerCount: count
+        });
+
+    }
+
+    update(){
+        var playerIndex= "players/player" +this.index;
+        database.ref(playerIndex).set({
+         name: this.name,
+         distance: this.distance
     });
-  }
+    }
+    static getPlayerInfo(){
+        var playerInfoRef=database.ref('players');
+        playerInfoRef.on("value", (data)=> { //this is an inline function that will be executed here only
+            allPlayers=data.val();
+        });
 
-  update(name){
-    var playerIndex = "player" + playerCount;
-    database.ref(playerIndex).set({
-      name:name
-    });
-  }
+    }
 }
